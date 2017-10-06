bcfc70be8081059be6109cc487906b40ad64c1e4
Bruno <bruno.barrette@gmail.com>
2017-10-05 21:27:18 -0400

    Premier draft pour le pattern Builder

---
+++ BuilderPattern/BuilderPattern.Tests/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern.Tests
+{
+    public class InvoiceBuilder
+    {
+        private Invoice _invoice;
+
+        public InvoiceBuilder() {
+            _invoice = new Invoice();
+        }
+
+        public InvoiceBuilder WithInvoiceNumber(int invoiceNumber) {
+            _invoice.InvoiceNumber = invoiceNumber;
+            return this;
+        }
+
+        public InvoiceBuilder WithClientName(string clientName) {
+            _invoice.ClientName = clientName;
+            return this;
+        }
+
+
+
+        public Invoice Build() {
+            return _invoice;
+        }
+    }
+
+    public class ProductBuilder {
+        private Product _product;
+
+        public ProductBuilder() {
+            
+        }
+
+        public Product Build() {
+            return _product;
+        }
+    }
+
+}
---
+++ BuilderPattern/BuilderPattern.Tests/InvoiceTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BuilderPattern.Tests {
+
+    [TestFixture]
+    public class InvoiceTest {
+        private InvoiceBuilder _invoiceBuilder;
+
+        [SetUp]
+        public void Setup() {
+            _invoiceBuilder = new InvoiceBuilder();
+            _invoiceBuilder.WithClientName("Mon client")
+                .WithInvoiceNumber(123);
+        }
+
+        [Test]
+        public void CalculateInvoiceTotalWithNoProducts() {
+
+        }
+
+        [Test]
+        public void CalculateInvoiceTotalWithOnlySimpleProducts() {
+
+        }
+
+        [Test]
+        public void CalculateInvoiceTotalWithOnlyConfigurableProducts() {
+
+        }
+
+        [Test]
+        public void CalculateInvoiceTotalWithSimpleAndConfigurableProducts() {
+
+        }
+    }
+}
